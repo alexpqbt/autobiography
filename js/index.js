a69cc5df672d644a4ae560f81606f2b0d250b13d
Andrei Fox <115098095+alexpqbt@users.noreply.github.com>
2023-11-26 22:56:17 +0800

    add scrolldown overlay to hide after scrolling

--- js/index.js
+++ js/index.js
@@ -11,3 +11,21 @@ const osInstance = OverlayScrollbars(document.querySelector('body'), {
         theme: 'os-theme-light',
     }
 });
+
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  var html = document.documentElement;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
+}
+
+const essayTitle = document.querySelector("section.essay h2");
+const scrolldownOverlay = document.querySelector(".scrolldown-overlay")
+
+window.addEventListener("scroll", function() {
+  isInViewport(essayTitle) ? scrolldownOverlay.style.visibility = "hidden" : scrolldownOverlay.style.visibility='visible'
+})
