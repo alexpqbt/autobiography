1887a66dffa44529450c902dd3dcd76f48394040
Andrei Fox <115098095+alexpqbt@users.noreply.github.com>
2023-12-10 05:37:31 +0800

    add checker if person is in mobile

--- js/index.js
+++ js/index.js
@@ -106,3 +106,18 @@ homeTitle.addEventListener("animationend", function () {
 });
 
 // window.onload = disableScroll()
+window.onload = () => {
+  if (navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)) {
+    document.querySelector("body").innerHTML = `
+      <div class="suggestion-mobile">
+        <p>Best experienced on desktop :3</p>
+      </div>
+    `
+  }
+}
